d053301f59538d76af436c519dab13f93254ad6e
rehanshakeel-websoftdeveloper <rehanshakeel.websoftdeveloper@gmail.com>
2025-10-23 17:00:10 +0530

    counter project files

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 22;
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    // setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Practice Project</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+    </>
+  )
+}
+
+export default App
+
+
